7c0b03650332839961dd3d71368da529267c6091
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2021-02-25 23:05:08 -0400

    Adicionar arquivos de projeto.

---
+++ Tutorial2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // variables
+    float annualSalary = 50000.99; // typeVariable nameVariable = valueVareiable
+    float monthlySalary = annualSalary / 12;
+    cout << "Your monthly salary is " << monthlySalary;
+    system("pause > 0");
+}
